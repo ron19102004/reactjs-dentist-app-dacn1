7e04eb473fa02fcf9c4fef8ea8b3ac97fd422e75
Your Name <you@example.com>
2025-04-08 00:08:19 +0700

    shadcn

--- src/roots/pages/user/App.tsx
+++ src/roots/pages/user/App.tsx
@@ -1,4 +1,5 @@
 import OllamaButton from "../../../components/ollama/button.ollama";
+import { Button } from "../../../components/ui/button";
 import SEOPageProvier from "../../../providers/seo.provider";
 
 function App() {
@@ -9,8 +10,11 @@ function App() {
       }}
     >
       <section className="relative min-w-screen min-h-screen">
-        <OllamaButton className="absolute bottom-[10%] right-[2%]"/>
+        <OllamaButton className="absolute bottom-[10%] right-[2%]" />
         <h1>User App</h1>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button>Click me</Button>
+        </div>
       </section>
     </SEOPageProvier>
   );
